6316e0dbcd221c7cd4a8165156a96be028986add
Paulo Vítor Araújo <33043117+araujovitorpaulo@users.noreply.github.com>
2024-02-04 22:50:11 -0300

    adição script.js e button onclick

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+function github() {
+  window.open('https:github.com/araujovitorpaulo', '_blank');
+}
+
+function mentor() {
+  window.open('https://www.frontendmentor.io/profile/araujovitorpaulo', '_blank');
+}
+
+function linkedin() {
+  window.open('https://www.linkedin.com/in/araujovitorpaulo/', '_blank');
+}
+
+function twitter() {
+  window.open('https://twitter.com/pvzera_', '_blank');
+}
+
+function insta() {
+  window.open('https://www.instagram.com/pvzera_', '_blank');
+}
+
