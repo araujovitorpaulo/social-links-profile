1f9206b580c4487f689d874f4548cb3fa623f6fb
Paulo Vítor Araújo <33043117+araujovitorpaulo@users.noreply.github.com>
2024-02-06 22:10:46 -0300

    script.js update

--- assets/script.js
+++ assets/script.js
@@ -1,44 +1,19 @@
-/*function github() {
-  window.open('https://github.com/araujovitorpaulo', '_blank');
-}
-
-function mentor() {
-  window.open('https://www.frontendmentor.io/profile/araujovitorpaulo', '_blank');
-}
-
-function linkedin() {
-  window.open('https://www.linkedin.com/in/araujovitorpaulo/', '_blank');
-}
-
-function twitter() {
-  window.open('https://twitter.com/pvzera_', '_blank');
-}
-
-function insta() {
-  window.open('https://www.instagram.com/pvzera_', '_blank');
-}*/
-
-
 function newTab(url) {
   window.open(url, '_blank');
 }
 
 const buttons = [
-  { id: 'b0', url: 'https://github.com/araujovitorpaulo' },
-  { id: 'b1', url: 'https://www.frontendmentor.io/profile/araujovitorpaulo' },
-  { id: 'b2', url: 'https://www.linkedin.com/in/araujovitorpaulo/' },
-  { id: 'b3', url: 'https://twitter.com/pvzera_' },
-  { id: 'b4', url: 'https://www.instagram.com/pvzera_' }
+  { url: 'https://github.com/araujovitorpaulo' },
+  { url: 'https://www.frontendmentor.io/profile/araujovitorpaulo' },
+  { url: 'https://www.linkedin.com/in/araujovitorpaulo/' },
+  { url: 'https://twitter.com/pvzera_' },
+  { url: 'https://www.instagram.com/pvzera_' }
 ];
 
-const buttonsContainer = document.getElementById('buttonsContainer');
+const buttonsElement = document.getElementsByTagName('button');
 
-buttons.forEach(button => {
-  const newButton = document.createElement('button');
-  newButton.id = button.id;
-  
-  newButton.addEventListener('click', function() {
-    newTab(button.url);
-  });
-
-});
+for (let i=0; i<buttonsElement.length; i++){
+  buttonsElement[i].addEventListener('click', function(){
+    newTab(buttons[i].url);
+  })
+}
